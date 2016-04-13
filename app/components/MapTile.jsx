985a605d36a9fbb8b4ea8265964bedbb798cf2b8
Kevin Hoffman <kehoffman@pivotal.io>
2016-04-13 16:25:48 -0400

    Removed unnecessary key from tile div

--- app/components/MapTile.jsx
+++ app/components/MapTile.jsx
@@ -4,7 +4,7 @@ export default ({tile}) => {
   var className = "maptile tile-" + tile.tile;
 
   return (
-    <div key={tile.id} className={className}>
+    <div className={className}>
       <img id={tile.sprite} src="/sprites/img_trans.gif"/>
     </div>
   )
